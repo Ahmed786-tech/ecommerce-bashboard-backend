2d0b27c50b8d59682db5363a59076532c9427bd4
Ahmed786-tech <ahmedhanifkhan786@gmail.com>
2025-06-12 00:43:19 +0500

    vercel config added

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ app.use(express.json());
 
 app.use(cors({
   origin: 'https://ecommerce-dashboard-frontend-cyan.vercel.app',
+  credentials: true
 }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://ecommerce-dashboard-frontend-cyan.vercel.app');
